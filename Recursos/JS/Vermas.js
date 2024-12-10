b7d893360aaa27ab8874dfa41649658e756d790b
BrandonJr20 <alexander.jncz@gmail.com>
2024-12-10 09:49:21 -0600

    Cambios 10-12-24

--- Recursos/JS/Vermas.js
+++ Recursos/JS/Vermas.js
@@ -1,18 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const card = document.querySelectorAll('.card');
-  
-    card.forEach(item => {
-      const img = item.querySelector('img');
-      const staticSrc = item.getAttribute('data-static');
-      const gifSrc = item.getAttribute('data-gif');
-  
-      item.addEventListener('mouseover', () => {
-        img.src = gifSrc;
-      });
-  
-      item.addEventListener('mouseout', () => {
-        img.src = staticSrc;
-      });
+  const card = document.querySelectorAll('.card');
+
+  card.forEach(item => {
+    const img = item.querySelector('img');
+    const staticSrc = item.getAttribute('data-static');
+    const gifSrc = item.getAttribute('data-gif');
+
+    item.addEventListener('mouseover', () => {
+      img.src = gifSrc;
+    });
+
+    item.addEventListener('mouseout', () => {
+      img.src = staticSrc;
     });
   });
-  
+
+});
+
+document.querySelector('.nav-toggle').addEventListener('click', function () {
+  const navbar = document.getElementById('navbar');
+  navbar.classList.toggle('active');
+});
+
